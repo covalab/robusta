c32e3c588b062555ea1611abfce8955c01bd651e
Minh Vuong <vuongxuongminh@gmail.com>
2023-03-17 23:38:00 +0700

    fix: coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,6 +6,10 @@ set -e
 
 dart pub global activate coverage
 
-dart test --coverage="coverage"
+dart pub global activate test
+
+mkdir -p coverage
+
+test --coverage="coverage"
 
 format_coverage --lcov --in=coverage --out=coverage.lcov --packages=.dart_tool/package_config.json --report-on=lib
