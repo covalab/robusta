414b308c1d494b35c08f46e8948ba24fb6f66ef1
Minh Vuong <vuongxuongminh@gmail.com>
2023-03-24 09:36:56 +0700

    feat!: init flutter should be call when boot instead

--- packages/flutter_robusta_hive/lib/src/extension.dart
+++ packages/flutter_robusta_hive/lib/src/extension.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:robusta_runner/robusta_runner.dart';
 
@@ -14,7 +15,11 @@ class FlutterHiveExtension implements Extension {
   final List<TypeAdapter> _typeAdapters;
 
   @override
-  Future<void> load(Configurator configurator) async {
+  void load(Configurator configurator) {
+    configurator.addBoot(_boot, priority: 8191);
+  }
+
+  Future<void> _boot(ProviderContainer container) async {
     await Hive.initFlutter();
 
     for (final adapter in _typeAdapters) {
