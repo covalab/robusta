4b10191be05e4c6586be046a2ba247280acb5dbe
qu0cquyen <quyen.lu@fastboy.net>
2023-04-12 11:29:34 -0400

    docs: fix typo

--- packages/robusta_firebase_core/example/test/main_test.dart
+++ packages/robusta_firebase_core/example/test/main_test.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:flutter_robusta_firebase_core_example/main.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:robusta_firebase_core_example/main.dart';
 
 void main() {
   group('test app', () {
