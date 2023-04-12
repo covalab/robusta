c8a8b94002d7e768e92438671e0b4d643077067e
Minh Vuong <vuongxuongminh@gmail.com>
2023-04-12 16:36:29 +0700

    feat: add Melos command to update cli versions

---
+++ scripts/generate_flutter_robusta_cli_version.dart
@@ -0,0 +1,29 @@
+/// Copied from https://github.com/invertase/flutterfire_cli/
+
+import 'dart:io' show Directory, File;
+import 'package:path/path.dart' show joinAll;
+import 'package:yaml/yaml.dart' show YamlMap, loadYaml;
+
+Future<void> main() async {
+  final outputPath = joinAll(
+    [
+      Directory.current.path,
+      'packages',
+      'flutter_robusta_cli',
+      'lib',
+      'version.g.dart'
+    ],
+  );
+  // ignore: avoid_print
+  print('Updating generated file $outputPath');
+  final pubspecPath = joinAll(
+    [Directory.current.path, 'packages', 'flutter_robusta_cli', 'pubspec.yaml'],
+  );
+  final yamlMap = loadYaml(File(pubspecPath).readAsStringSync()) as YamlMap;
+  final currentVersion = yamlMap['version'] as String;
+  final fileContents =
+      "// This file is generated. Do not manually edit.\nString cliVersion = '$currentVersion';\n";
+  await File(outputPath).writeAsString(fileContents);
+  // ignore: avoid_print
+  print('Updated version to $currentVersion in generated file $outputPath');
+}
