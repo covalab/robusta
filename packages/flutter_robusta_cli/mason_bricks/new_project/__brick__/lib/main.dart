16a95c0775fbef87df66e122d61b770bc6b34583
Minh Vuong <vuongxuongminh@gmail.com>
2023-04-17 17:21:19 +0700

    feat: completes new project brick

--- packages/flutter_robusta_cli/mason_bricks/new_project/__brick__/lib/main.dart
+++ packages/flutter_robusta_cli/mason_bricks/new_project/__brick__/lib/main.dart
@@ -1,1 +1,12 @@
-void main() => print('hello world');
+import 'package:flutter_robusta/flutter_robusta.dart';
+import 'package:{{project_name}}/robusta/app.dart';
+
+Future<void> main() async {
+  final runner = Runner(
+    extensions: [
+      appExtension(),
+    ],
+  );
+
+  await runner.run();
+}
