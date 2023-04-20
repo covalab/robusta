d93b3cfb4db0b206f19a52ede030a44bafd7e044
Minh Vuong <vuongxuongminh@gmail.com>
2023-04-20 13:52:14 +0700

    feat: add Melos script for generating bricks

---
+++ scripts/generate_mason_bricks.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+Future<void> main() async {
+  final stack = {
+    'flutter_robusta_cli': [
+      'new_project',
+      'add_auth',
+      'add_dio',
+      'add_graphql',
+      'add_hive',
+      'add_firebase_core',
+    ],
+  };
+
+  for (final entry in stack.entries) {
+    final package = entry.key;
+
+    for (final brick in entry.value) {
+      await Process.run(
+        'mason',
+        [
+          'bundle',
+          './packages/$package/mason_bricks/$brick',
+          '-t',
+          'dart',
+          '-o',
+          './packages/$package/lib/mason_bricks',
+        ],
+        runInShell: true,
+      );
+    }
+  }
+
+  await Future.wait([stderr.close(), stdout.close()]);
+
+  exit(0);
+}
