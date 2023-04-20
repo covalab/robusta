ce4ccfb91ef2ba7ec38cbb0700f000c4977616eb
Minh Vuong <vuongxuongminh@gmail.com>
2023-04-20 17:29:24 +0700

    test: flag-off init Hive

--- packages/flutter_robusta_graphql/test/src/extension_test.dart
+++ packages/flutter_robusta_graphql/test/src/extension_test.dart
@@ -1,10 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter_robusta/flutter_robusta.dart';
 import 'package:flutter_robusta_graphql/flutter_robusta_graphql.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 
+import 'fake.dart';
 import 'stub.dart';
 
 void main() {
+  setUp(() async {
+    final instance = PathProviderPlatform.instance = FakePathProviderPlatform();
+
+    final path = await instance.getApplicationDocumentsPath();
+    final dir = Directory(path!);
+
+    if (dir.existsSync()) {
+      dir.deleteSync(recursive: true);
+    }
+  });
+
   group('flutter graphql extension', () {
     test('can initialize', () {
       expect(
@@ -74,6 +89,13 @@ void main() {
         ],
       );
 
+      final hivePath =
+          await PathProviderPlatform.instance.getApplicationDocumentsPath();
+
+      final documentDir = Directory(hivePath!);
+
+      expect(documentDir.existsSync(), isFalse);
+
       await expectLater(tester.runAsync(runner.run), completes);
 
       expect(client, isA<GraphQLClient>());
@@ -82,6 +104,8 @@ void main() {
         tester.allWidgets.any((element) => element is GraphQLProvider),
         isTrue,
       );
+
+      expect(documentDir.existsSync(), isTrue);
     });
 
     testWidgets('can flag-off graphql provider', (tester) async {
@@ -136,5 +160,31 @@ void main() {
         expect(testService!.graphQLClient, isA<GraphQLClient>());
       },
     );
+
+    testWidgets('can disable init hive on boot', (tester) async {
+      final runner = Runner(
+        extensions: [
+          FlutterAppExtension(routerSettings: RouterSettings()),
+          FlutterGraphQLExtension(
+            initHive: false,
+            settings: GraphQLClientSettings(
+              linkFactory: (c) => Link.from([HttpLink('')]),
+              cacheFactory: (c) => GraphQLCache(),
+            ),
+          ),
+        ],
+      );
+
+      final hivePath =
+          await PathProviderPlatform.instance.getApplicationDocumentsPath();
+
+      final documentDir = Directory(hivePath!);
+
+      expect(documentDir.existsSync(), isFalse);
+
+      await expectLater(tester.runAsync(runner.run), completes);
+
+      expect(documentDir.existsSync(), isFalse);
+    });
   });
 }
